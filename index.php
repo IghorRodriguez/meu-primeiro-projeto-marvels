9ced1efb0872a2e713d7666b607a94ed3c876930
Ighor Rodrigues <ighormurdock07@gmail.com>
2023-05-24 22:05:42 -0300

    Iniciando meu primeiro projeto

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TheMarvels</title>
+        <link rel="stylesheet" href="css/themarvel.css">
+    </head>
+    <body>
+    </body>
+</html>
