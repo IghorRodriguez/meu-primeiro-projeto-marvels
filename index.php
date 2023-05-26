6b55b5352ad1acc32ab47af50a260546613baa23
Ighor Rodrigues <ighormurdock07@gmail.com>
2023-05-26 14:22:27 -0300

    Meu primeiro projeto

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title>TheMarvels</title>
-        <link rel="stylesheet" href="css/themarvel.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
+
+    <?php include('inc/marvels.php'); ?>
+
     </body>
 </html>
