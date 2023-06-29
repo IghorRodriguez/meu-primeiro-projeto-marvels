187ca7ba2528ff05685eea5c6e05a3a36efe69c9
Ighor Rodrigues <ighormurdock07@gmail.com>
2023-06-29 18:30:49 -0300

    Meu primeiro projeto concluido

---
+++ backend/conexao-com-mysql.php
@@ -0,0 +1,20 @@
+<?php
+//Função Nativa do PHP : ini_set
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once 'credenciais.php';
+
+try {
+    // o código que estiver aqui, se houver alguma exceção ele interrompe essa execução
+
+    // cria uma instância da classe PDO
+    $conexão = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+    //configuro o PDO para um modo de exibição de lançamento de exceção 
+    $conexão->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem sucedida";
+}catch(PDOException $excecao) {
+    //e inicia a execução do que estiver aqui
+    echo "A conexão falhou! Mensagem: ".$excecao->getMessage();
+}
---
+++ backend/credenciais.php
@@ -0,0 +1,6 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '123987';
+$databasename = 'igor23_database';
